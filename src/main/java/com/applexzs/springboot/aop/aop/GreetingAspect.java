4d75223c4a211d52d96d0c461090623916a31cb5
Applexzs <reverseapplemw2@gmail.com>
2026-04-14 18:02:10 -0600

    add Around AOP

--- src/main/java/com/applexzs/springboot/aop/aop/GreetingAspect.java
+++ src/main/java/com/applexzs/springboot/aop/aop/GreetingAspect.java
@@ -2,6 +2,7 @@ package com.applexzs.springboot.aop.aop;
 
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -41,5 +42,22 @@ public class GreetingAspect {
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
         log.info("Despues lanzar la excepcion: " + method + " con los argumentos " + args);
+    }
+
+    @Around("execution(String com.applexzs.springboot.aop.services.IGreetingService.*(..))")
+    public Object loggerAround(ProceedingJoinPoint joinPoint) {
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        Object result = null;
+        try {
+            log.info("El metodo " + method + "() con los parametros  " + args);
+            result = joinPoint.proceed();
+            log.info("El metodo " + method + "() retorna el resultado: " + result);
+            return result;
+        } catch (Throwable e) {
+            e.printStackTrace(System.out);
+            log.info("Error en lla llamada del metodo " + method + "()");
+        }
+        return result;
     }
 }
